25bf1905af02eb6d5b49726f34e7258cb4c7c8a0
Park JaeHong <blogerhong@naver.com>
2025-06-01 18:11:42 +0900

    add : git init

---
+++ src/main/java/com/jhlab/ninety/NinetyApplication.java
@@ -0,0 +1,13 @@
+package com.jhlab.ninety;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NinetyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NinetyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jhlab/ninety/NinetyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jhlab.ninety;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NinetyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
