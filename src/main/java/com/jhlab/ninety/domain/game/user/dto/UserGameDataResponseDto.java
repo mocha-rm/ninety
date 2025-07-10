6ac6a626ffe06474df25eb13d86b660dcd1871c0
Park JaeHong <blogerhong@naver.com>
2025-07-10 23:47:54 +0900

    feat : 유저 게임 데이터 관련 기능 API 추가

--- src/main/java/com/jhlab/ninety/domain/game/user/dto/UserGameDataResponseDto.java
+++ src/main/java/com/jhlab/ninety/domain/game/user/dto/UserGameDataResponseDto.java
@@ -1,4 +1,25 @@
 package com.jhlab.ninety.domain.game.user.dto;
 
+import com.jhlab.ninety.domain.game.user.entity.UserGameData;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
 public class UserGameDataResponseDto {
+    private final Long id;
+    private final Long userId;
+    private final int coins;
+    private final int level;
+    private final int experience;
+
+    public static UserGameDataResponseDto toDto(UserGameData userGameData) {
+        return new UserGameDataResponseDto(
+                userGameData.getId(),
+                userGameData.getUserId(),
+                userGameData.getCoins(),
+                userGameData.getLevel(),
+                userGameData.getExperience()
+        );
+    }
 }
---
+++ src/main/java/com/jhlab/ninety/domain/game/user/service/UserGameDataServiceImpl.java
@@ -0,0 +1,46 @@
+package com.jhlab.ninety.domain.game.user.service;
+
+import com.jhlab.ninety.domain.game.user.dto.UserGameDataResponseDto;
+import com.jhlab.ninety.domain.game.user.entity.UserGameData;
+import com.jhlab.ninety.domain.game.user.repository.UserGameDataRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class UserGameDataServiceImpl implements UserGameDataService {
+    private final UserGameDataRepository userGameDataRepository;
+
+    @Override
+    @Transactional
+    public UserGameDataResponseDto createUserGameData(Long userId) {
+        UserGameData data = new UserGameData(
+                userId,
+                0,
+                1,
+                0
+        );
+
+        return UserGameDataResponseDto.toDto(data);
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public UserGameDataResponseDto getUserGameData(Long userId) {
+        UserGameData data = userGameDataRepository.findByUserId(userId);
+        return UserGameDataResponseDto.toDto(data);
+    }
+
+    @Override
+    @Transactional
+    public UserGameDataResponseDto updateUserGameData(Long userId, int coins, int level, int experience) {
+        UserGameData data = userGameDataRepository.findByUserId(userId);
+
+        data.updateCoins(coins);
+        data.updateLevel(level);
+        data.updateExperience(experience);
+
+        return UserGameDataResponseDto.toDto(data);
+    }
+}
